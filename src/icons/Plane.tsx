a7605acb605c0ee088439355b315230018575434
Ruhul <ruhulinfo50@gmail.com>
2024-05-27 11:30:13 +0600

    Add icons

---
+++ src/icons/Plane.tsx
@@ -0,0 +1,20 @@
+function Plane() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+    >
+      <path
+        fill="currentColor"
+        fill-rule="evenodd"
+        d="M14.99 18.103v3.845a1.043 1.043 0 0 0 .288.756.976.976 0 0 0 .723.296.96.96 0 0 0 .722-.296c.195-.203.31-.472.321-.756v-3.845l5.456-3.517v-1.38l-5.456 1.742v-3.845l1.348-1.051V9l-2.376.69L13.61 9v1.052l1.38 1.051v3.845L9.5 13.207v1.38l5.49 3.516Z"
+        clip-rule="evenodd"
+      ></path>
+    </svg>
+  );
+}
+
+export default Plane;
